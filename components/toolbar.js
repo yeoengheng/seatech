da7a26b5981f2e3cc8a0f52f1e22930c9811ab03
Eng Heng Yeo <enghengyeo12@hotmail.com>
2022-10-06 21:10:54 +0800

    3rd adjustment to responsiveness

--- components/toolbar.js
+++ components/toolbar.js
@@ -3,7 +3,7 @@ import { Input, Flex, Spacer, Select, HStack} from "@chakra-ui/react";
 const Toolbar = ()=>{
     return(
         <HStack w="auto" spacing={1}>
-                <Input  size={'sm'} w={300}  placeholder="Search"/>
+                <Input  size={'sm'} w='auto'  placeholder="Search"/>
                 <Spacer/>
                 <Select size={'sm'} maxW={130} placeholder='Category'>
                     <option value="SaaS">SaaS</option>
